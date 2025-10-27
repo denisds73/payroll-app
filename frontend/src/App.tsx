3647e365339ca7c2930ca3748e4ef1f56c947323
Flavio Denis <flavio.d@commerceiq.ai>
2025-10-27 19:20:13 +0530

    build button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Button from './components/ui/Button';
 
 function App() {
-  return <div className="hi"></div>;
+  return (
+    <div className="flex gap-4 p-6">
+      <Button>Primary</Button>
+    </div>
+  );
 }
 
 export default App;
